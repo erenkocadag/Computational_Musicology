284326bb2c72072bf03bfae31f164636650d4eaf
erenkocadag <72503116+erenkocadag@users.noreply.github.com>
2023-03-05 04:25:19 +0100

    Start on hw

--- plotting.R
+++ plotting.R
@@ -4,6 +4,7 @@ library(spotifyr)
 library(ggplot2)
 library(showtext)
 library(plotly)
+library(compmus)
 
 font_add_google("Inter", "Inter")
 showtext_auto()
@@ -16,12 +17,16 @@ showtext_auto()
 
 # retrieving the audio features of my corpus
 baba <- get_playlist_audio_features("", "37i9dQZF1DX9azIhpBmNbd")
+eski <- get_playlist_audio_features("", "12VU80QKYGednqNghHrWNW")
 beskr <- get_playlist_audio_features("", "37i9dQZF1DXciCKvzkUxfS")
 beskp <- get_playlist_audio_features("", "37i9dQZF1DXduWUvte9ZYi?")
 
-# merging the playlists and removing irrelevant features to make plots
+# merging the playlists and other preprocessing
+eski <- mutate(eski, playlist_name = "Arabesk")
+baba <- mutate(baba, playlist_name = "Arabesk")
 merged <- merge(beskr, beskp, all = TRUE)
 merged <- merge(merged, baba, all = TRUE)
+merged <- merge(merged, eski, all = TRUE)
 merged <- merged[,!names(merged) %in% c('playlist_img','playlist_owner_name',
                                         'playlist_owner_id','playlist_id',
                                         'track.id','added_at',
@@ -42,7 +47,7 @@ merged <- merged[,!names(merged) %in% c('playlist_img','playlist_owner_name',
                                         'track.album.available_markets')]
 
 # the actual plotting
-merged %>%
+val <- merged %>%
   group_by(playlist_name) %>%
   summarize(mean_valence = mean(valence)) %>%
   ggplot(aes(playlist_name, mean_valence, fill = playlist_name)) +
@@ -57,7 +62,9 @@ merged %>%
            panel.grid.minor = element_blank()) +
     scale_y_continuous(breaks=seq(0,1,1))
 
-ggplot(merged, aes(danceability, acousticness)) +
+ggplotly(val)
+
+scatter <- ggplot(merged, aes(danceability, acousticness)) +
   geom_point() +
   # geom_smooth() +
   coord_cartesian(ylim = c(0,1), xlim = c(0,1)) +
@@ -69,8 +76,12 @@ ggplot(merged, aes(danceability, acousticness)) +
   labs(x = "Danceability", y = "Acousticness", title = "Classic Arabesk songs seem to be more acoustic and less danceable than the two subgenres") +
   facet_wrap(~playlist_name)
 
-p <- ggplot(merged, aes(tempo, track.popularity, color=playlist_name)) +
-        geom_boxplot(notch=TRUE) +
+ggplotly(scatter)
+
+
+pop <- ggplot(merged, aes(tempo, track.popularity, color=playlist_name)) +
+        geom_violin() +
+        geom_boxplot() +
         labs(x = "Tempo", y = "Track popularity", title = "Older Arabesk music currently is less popular than the subgenres") +
         theme_bw() +
         theme(
@@ -80,5 +91,46 @@ p <- ggplot(merged, aes(tempo, track.popularity, color=playlist_name)) +
           panel.grid.minor = element_blank()) +
         facet_wrap(~playlist_name)
 
-ggplotly(p)
+fig <- plot_ly(y = merged$playlist_name, x = merged$track.popularity, type = "box", color = ~merged$playlist_name)
+fig <- fig %>% layout(title = "Older Arabesk music currently is less popular than the subgenres", xaxis = list(title="Track popularity"), yaxis = list(showticklabels=FALSE))
+
+fig
+
+muslum <-
+  get_tidy_audio_analysis("4cHrCNJTdMfbtY0fjc5ged") |> # Change URI.
+  compmus_align(bars, segments) |>                     # Change `bars`
+  select(bars) |>                                      #   in all three
+  unnest(bars) |>                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  )
+
+chroma1 <- muslum |>
+  compmus_gather_timbre() |>
+  ggplot(
+    aes(
+      x = start + duration / 2,
+      width = duration,
+      y = basis,
+      fill = value
+    )
+  ) +
+  geom_tile() +
+  labs(x = "Time (s)", y = NULL, fill = "Magnitude") +
+  scale_fill_viridis_c() +                              
+  theme_classic()
+
+
+#ggplotly(chroma1)
          
